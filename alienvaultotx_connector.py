02bac4863170140c5fcc913616a19913c0e10105
Ricardo Munoz <rmunoz@rmunoz-mbp-9d822.sv.splunk.com>
2019-03-11 13:16:20 -0700

    updated copyrights

--- alienvaultotx_connector.py
+++ alienvaultotx_connector.py
@@ -1,4 +1,8 @@
-# -----------------------------------------
+# File: alienvaultotx_connector.py
+# Copyright (c) 2019 Splunk Inc.
+#
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.# -----------------------------------------
 # Phantom sample App Connector python file
 # -----------------------------------------
 
